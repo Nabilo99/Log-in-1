29b5f2315f354cfd25890dd15c9eafba6a07ee47
nabilo99 <khaled.mfdkr@gmail.com>
2022-02-04 21:36:25 +0100

    omk sus

---
+++ login.js
@@ -0,0 +1,11 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if (username=="aziz" && password=="aziz");
+{
+alert("login succesfully");
+return false;
+}
+
+}
